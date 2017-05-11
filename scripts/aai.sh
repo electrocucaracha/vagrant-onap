f98c72e56bd71c22d7d600c9943243b6ea76bcb4
Victor Morales <victor.morales@intel.com>
2017-05-11 12:23:36 -0500

    Refactor and fix AAI scripts

--- scripts/aai.sh
+++ scripts/aai.sh
@@ -8,10 +8,7 @@ source /var/onap/aai
 configure_dns
 create_configuration_files
 install_dev_tools
-install_java
-install_maven
 install_docker_engine
-install_docker_compose
 
 configure_service aai_serv.sh
 init_aai
